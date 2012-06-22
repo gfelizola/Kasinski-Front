556c9ce8f2d5452a05060dcf4a8067172e461c56
Gustavo Felizola <gfelizola@gmail.com>
2012-06-22 00:47:09 -0300

    produtos com caracteristicas, downloads, especificacoes 100% //TODO item de especificação com nome E valor grande quebrando em linhas

--- produtos.php
+++ produtos.php
@@ -39,8 +39,33 @@
 					</div>
 				</div>
 			</div>
-			<div class="area especificacoes hidden"></div>
-			<div class="area caracteristicas">
+			<div class="area especificacoes">
+				<div class="moto">
+					<ul class="botoes">
+						<li class="lupa motor"><a href="#">Motor</a></li>
+						<li class="lupa rodas"><a href="#">Rodas</a></li>
+						<li class="lupa painel"><a href="#">Painel</a></li>
+					</ul>
+				</div>
+				<div class="descricao">
+					<img src="img/produtos_caracs_conteudo_seta.png" alt="" class="setinha" />
+					<div class="box">
+						<h3>MOTOR</h3>
+						<ul>
+							<li>
+								<p><span>Capacidade volumetrica</span><span class="valor">149 cm³</span></p>
+							</li>
+							<li>
+								<p><span>Torque Maximo</span><span class="valor">1,24 kgf.m a 6.000 rpm</span></p>
+							</li>
+							<li>
+								<p><span>Diametro do cilindro</span><span class="valor">62 mm</span></p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="area caracteristicas hidden">
 				<div class="moto">
 					<ul class="botoes">
 						<li class="lupa motor"><a href="#">Motor</a></li>
@@ -58,7 +83,26 @@
 				</div>
 			</div>
 			<div class="area galeria hidden"></div>
-			<div class="area downloads hidden"></div>
+			<div class="area downloads hidden">
+				<div class="wallpapers">
+					<h2>Papéis de parede</h2>
+					<ul>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+						<li><a href="#"><img src="img/produtos_wallpaper_fake.png" /></a></li>
+					</ul>
+				</div>
+				<div class="screensavar">
+					<h2>Screensavers</h2>
+					<a href="#" class="imagem"><img src="img/produtos_downloads_monitor_over.png" class="over" /></a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut dapibus arcu.</p>
+					<a href="#" class="bt-download">Download</a>
+				</div>
+			</div>
 			<a href="#" class="seta direita">Próxima</a>
 		</div>
 		<div class="menu-produtos">
